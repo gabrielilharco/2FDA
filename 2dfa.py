0b67b972e8eb1c8d7ec621d173c812035208a1f3
Lucas Muller <lucasmullerm@gmail.com>
2015-09-09 20:36:48 -0300

    State, Tape, Transition

--- 2dfa.py
+++ 2dfa.py
@@ -1,13 +1,54 @@
 #python3
+import enum
 
-class Node():
-	pass
+class State():
+	def __init__(self, value, accept):
+		self.value = value
+		self.transitions = {}
+		self.accept = accept
 
-class Edge():
-	pass
+	def addTransition(self, transition):
+		self.transitions[transition.symbol] = transition
+
+	def step(self, symbol):
+		try:
+			transition = transitions[symbol]
+			return transition.nextState, transition.direction
+		except KeyError as e:
+			raise e
+
+class Transition():
+	class Direction(enum.Enum):
+		RIGHT = 1
+		LEFT  = 2
+
+	def __init__(self, symbol, nextState, direction):
+		self.symbol    = symbol
+		self.nextState = nextState
+		self.direction = direction
 
 class Tape():
-	pass
+	def __init__(self, cells):
+		self.cells    = cells
+		self.position = 0
 
-class Node():
+	def head(self):
+		return cells[position]
+
+	def headPosition(self):
+		return position
+
+	def reachEnd(self):
+		position == -1 or position == len(cells)
+	
+	def update(self, direction):
+		if reachEnd():
+			raise Exception('End of computation.')
+		if direction == Transition.Direction.RIGHT:
+			position += 1
+		else:
+			position -= 1
+
+
+class Automaton():
 	pass
